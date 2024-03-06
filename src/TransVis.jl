59d077ae7e60e2e09f5c3f0a32186c479b427b04
Baldwin Nsonga <nsonga@informatik.uni-leipzig.de>
2024-03-05 20:40:47 -0700

    style: apply code formatter

--- src/TransVis.jl
+++ src/TransVis.jl
@@ -28,19 +28,19 @@ include("processing.jl")
 export go
 
 
-function link_cameras_lscene(f; step=.01)
+function link_cameras_lscene(f; step = 0.01)
     scenes = filter(x -> x isa LScene, f.content)
     cameras = map(x -> cameracontrols(x.scene), scenes)
-  
+
     for i ∈ 1:length(cameras)
-      on(cameras[i].eyeposition) do eye
-        for j ∈ 1:length(cameras)
-          i == j && continue
-          if sum(abs, eye - cameras[j].eyeposition[]) > step
-            update_cam!(scenes[j].scene, cameras[i])
-          end
+        on(cameras[i].eyeposition) do eye
+            for j ∈ 1:length(cameras)
+                i == j && continue
+                if sum(abs, eye - cameras[j].eyeposition[]) > step
+                    update_cam!(scenes[j].scene, cameras[i])
+                end
+            end
         end
-      end
     end
     f
 end
@@ -48,13 +48,15 @@ end
 
 
 function angle(a, b)
-    return acosd(clamp(a⋅b/(norm(a)*norm(b)), -1, 1))
+    return acosd(clamp(a ⋅ b / (norm(a) * norm(b)), -1, 1))
 end
 
-function fractionalAnisotropy( ev::Vector{Float64} )
-    meanEV = (ev[1] + ev[2] + ev[3])/3.0
-    a = sqrt( (ev[1] - meanEV)^2 + (ev[2] - meanEV)^2 + (ev[3] - meanEV)^2  ) / sqrt( ev[1]^2 + ev[2]^2 + ev[3]^2  )
-    return sqrt(3.0/2.0) * a  
+function fractionalAnisotropy(ev::Vector{Float64})
+    meanEV = (ev[1] + ev[2] + ev[3]) / 3.0
+    a =
+        sqrt((ev[1] - meanEV)^2 + (ev[2] - meanEV)^2 + (ev[3] - meanEV)^2) /
+        sqrt(ev[1]^2 + ev[2]^2 + ev[3]^2)
+    return sqrt(3.0 / 2.0) * a
 end
 
 #Runs the logic
@@ -68,12 +70,12 @@ end
 #     plotWindow = Figure()
 #     molWindow = Figure()
 
-   
+
 #     lsceneLeft = LScene(molWindow[1:3, 1:3], show_axis=false, scenekw = (backgroundcolor = :whitesmoke, clear = true))
 #     lsceneRight = LScene(molWindow[1:3, 4:6], show_axis=false, scenekw = (backgroundcolor = :whitesmoke, clear = true))
 #     #lscenec = LScene(fig[1:2, 7:9], show_axis=false, scenekw = (backgroundcolor = :whitesmoke, clear = true))
 #     #lscene2 = LScene(fig[3:4, 1:3], show_axis=false, scenekw = (backgroundcolor = :whitesmoke, clear = true))
-    
+
 #     axI1 = Axis(plotWindow[1:2, 1:4], xlabel = "Atom Number", ylabel = "Invariant 1")
 #     axI2 = Axis(plotWindow[3:4, 1:4], xlabel = "Atom Number", ylabel = "Invariant 2")
 #     axI3 = Axis(plotWindow[5:6, 1:4], xlabel = "Atom Number", ylabel = "Invariant 3")
@@ -93,154 +95,165 @@ end
 #     f=current_figure()
 
 #     g=grid(screen)
-    
+
 #     #g[1,2]=GtkButton("Generate new random plot")
-    
+
 #     #function gen_cb(b)
 #     #    empty!(ax)
 #     #    lines!(ax,rand(10))
 #     #end
-    
+
 #     signal_connect(gen_cb,g[1,2],"clicked")
 # end
 
 
 
-function go() 
+function go()
 
 
     plotWindow = Figure()
     molWindow = Figure()
 
-   
-    lsceneLeft = LScene(molWindow[1:3, 1:3], show_axis=false, scenekw = (backgroundcolor = :whitesmoke, clear = true))
-    lsceneRight = LScene(molWindow[1:3, 4:6], show_axis=false, scenekw = (backgroundcolor = :whitesmoke, clear = true))
-    
+
+    lsceneLeft = LScene(
+        molWindow[1:3, 1:3],
+        show_axis = false,
+        scenekw = (backgroundcolor = :whitesmoke, clear = true),
+    )
+    lsceneRight = LScene(
+        molWindow[1:3, 4:6],
+        show_axis = false,
+        scenekw = (backgroundcolor = :whitesmoke, clear = true),
+    )
+
     axI1 = Axis(plotWindow[1:2, 1:4], xlabel = "Atom Number", ylabel = "Invariant 1")
     axI2 = Axis(plotWindow[3:4, 1:4], xlabel = "Atom Number", ylabel = "Invariant 2")
     axI3 = Axis(plotWindow[5:6, 1:4], xlabel = "Atom Number", ylabel = "Invariant 3")
-    axDR = Axis(plotWindow[1:6, 5:7],  title = "t-SNE")
+    axDR = Axis(plotWindow[1:6, 5:7], title = "t-SNE")
 
 
-     stateDataPath = "/Users/Bote/Documents/ASU/state_data copy/"
-     sequencePath = "/Users/Bote/Documents/ASU/state_data copy/seq.txt"
-     transitionPath = "/Users/Bote/Documents/ASU/nano_pt_labels.pickle"
+    stateDataPath = "/Users/Bote/Documents/ASU/state_data copy/"
+    sequencePath = "/Users/Bote/Documents/ASU/state_data copy/seq.txt"
+    transitionPath = "/Users/Bote/Documents/ASU/nano_pt_labels.pickle"
 
     combinedData = getDataSets(stateDataPath, sequencePath, transitionPath)
-    
-    transitionInvariants1 = combinedData["transitionInvariants1"] 
-    transitionInvariants2 = combinedData["transitionInvariants2"] 
-    transitionInvariants3= combinedData["transitionInvariants3"] 
 
-    transitionRefPositions = combinedData["transitionRefPositions"]  
-    transitionLabels = combinedData["transitionLabels"] 
+    transitionInvariants1 = combinedData["transitionInvariants1"]
+    transitionInvariants2 = combinedData["transitionInvariants2"]
+    transitionInvariants3 = combinedData["transitionInvariants3"]
+
+    transitionRefPositions = combinedData["transitionRefPositions"]
+    transitionLabels = combinedData["transitionLabels"]
 
 
 
     atoms = [1:1:length(values(transitionInvariants1) |> first);]
 
- #   balltree = BallTree(data, Minkowski(3.5); reorder = false)
+    #   balltree = BallTree(data, Minkowski(3.5); reorder = false)
 
-    lines!.( axI1, Ref(atoms), values(transitionInvariants1), alpha=0.01 )        
-    lines!.( axI2, Ref(atoms), values(transitionInvariants2), alpha=0.01 )
-    lines!.( axI3, Ref(atoms), values(transitionInvariants3), alpha=0.01 )
-
-
-   featureVectorMatrix = zeros( length( values(transitionInvariants1)),length( values(transitionInvariants1)|> first ) * 3 )
-   labels = zeros( length( values(transitionInvariants1)) )
+    lines!.(axI1, Ref(atoms), values(transitionInvariants1), alpha = 0.01)
+    lines!.(axI2, Ref(atoms), values(transitionInvariants2), alpha = 0.01)
+    lines!.(axI3, Ref(atoms), values(transitionInvariants3), alpha = 0.01)
 
 
-   mapNameToIdx = Dict{String, Int64}()
-   mapIdxToName = Vector{String}()
+    featureVectorMatrix = zeros(
+        length(values(transitionInvariants1)),
+        length(values(transitionInvariants1) |> first) * 3,
+    )
+    labels = zeros(length(values(transitionInvariants1)))
 
-   row = 1
-   for (transition, invariants1) in transitionInvariants1
+
+    mapNameToIdx = Dict{String,Int64}()
+    mapIdxToName = Vector{String}()
+
+    row = 1
+    for (transition, invariants1) in transitionInvariants1
         mapNameToIdx[transition] = row
         push!(mapIdxToName, transition)
 
         col = 1
         for invariant in invariants1
             featureVectorMatrix[row, col] = invariant |> abs
-            col = col +1
+            col = col + 1
         end
 
-        col = length( values(transitionInvariants1)|> first ) + 1
+        col = length(values(transitionInvariants1) |> first) + 1
         for invariant in transitionInvariants2[transition]
             featureVectorMatrix[row, col] = invariant |> abs
-            col = col +1
+            col = col + 1
         end
 
-        col = length( values(transitionInvariants1)|> first )*2 + 1
+        col = length(values(transitionInvariants1) |> first) * 2 + 1
         for invariant in transitionInvariants3[transition]
             featureVectorMatrix[row, col] = invariant |> abs
-            col = col +1
+            col = col + 1
         end
 
 
         #@show transition
         splitToStates = split(transition, ">")
-        flippedName = splitToStates[2]*">"*splitToStates[1]
+        flippedName = splitToStates[2] * ">" * splitToStates[1]
 
-       # @show flippedName
+        # @show flippedName
 
         if haskey(transitionLabels, transition)
             labels[row] = transitionLabels[transition]
         elseif haskey(transitionLabels, flippedName)
             labels[row] = transitionLabels[flippedName]
         else
-            @show "Transition Label not found for " * transition 
+            @show "Transition Label not found for " * transition
         end
 
-       row = row + 1
-
-   end
-   
-   #@show featureVectorMatrix[100,:]
-
-
-
-
-   selectedTransition = Observable{String}("1>3")
-
-   #rescale(A; dims=1) = (A .- mean(A, dims=dims)) ./ max.(std(A, dims=dims), eps())
-   #featureVectorMatrix = featureVectorMatrix |> rescale
-
-   #tsne(X, ndim, reduce_dims, max_iter, perplexit; [keyword arguments])
-   @time Y = tsne(featureVectorMatrix[:,:], 2, 20, 20, 40.0; );
-   tsnePlot = scatter!(axDR, Y, color=labels, colormap = (:viridis, 1.0))
-
-   on(events(plotWindow).mousebutton, priority = 2) do event
-    if event.button == Mouse.left && event.action == Mouse.press
-        # Delete marker
-        plt, i = pick(axDR)  
-        if plt == tsnePlot
-            # deleteat!(positions[], i)
-            selectedTransition[] = mapIdxToName[i]
-            notify(selectedTransition)
-            @show plt
-            @show i
-            @show selectedTransition[]
-            @show mapNameToIdx[mapIdxToName[i]]
-
-            @show Y[mapNameToIdx[mapIdxToName[i]],:]
-            #scatter!(axDR , Y[mapNameToIdx[mapIdxToName[i]],:], color=:black)
-            return Consume(true)
-        end
-        return Consume(false)
+        row = row + 1
 
     end
-    # return Consume(false)
-end
+
+    #@show featureVectorMatrix[100,:]
+
+
+
+
+    selectedTransition = Observable{String}("1>3")
+
+    #rescale(A; dims=1) = (A .- mean(A, dims=dims)) ./ max.(std(A, dims=dims), eps())
+    #featureVectorMatrix = featureVectorMatrix |> rescale
+
+    #tsne(X, ndim, reduce_dims, max_iter, perplexit; [keyword arguments])
+    @time Y = tsne(featureVectorMatrix[:, :], 2, 20, 20, 40.0;)
+    tsnePlot = scatter!(axDR, Y, color = labels, colormap = (:viridis, 1.0))
+
+    on(events(plotWindow).mousebutton, priority = 2) do event
+        if event.button == Mouse.left && event.action == Mouse.press
+            # Delete marker
+            plt, i = pick(axDR)
+            if plt == tsnePlot
+                # deleteat!(positions[], i)
+                selectedTransition[] = mapIdxToName[i]
+                notify(selectedTransition)
+                @show plt
+                @show i
+                @show selectedTransition[]
+                @show mapNameToIdx[mapIdxToName[i]]
+
+                @show Y[mapNameToIdx[mapIdxToName[i]], :]
+                #scatter!(axDR , Y[mapNameToIdx[mapIdxToName[i]],:], color=:black)
+                return Consume(true)
+            end
+            return Consume(false)
+
+        end
+        # return Consume(false)
+    end
 
 
     sequence = getSequence(sequencePath)
 
     transitionSequence = Vector{String}()
 
-    for sequenceStep in 1:(length(sequence)-1)
+    for sequenceStep = 1:(length(sequence)-1)
         currentState = sequence[sequenceStep]
         nextState = sequence[sequenceStep+1]
-        transitionName = currentState*">"*nextState
+        transitionName = currentState * ">" * nextState
         push!(transitionSequence, transitionName)
 
     end
@@ -248,28 +261,44 @@ end
 
     sliderTransition = SliderGrid(
         molWindow[4, 1:6],
-        (label= "Transition", range = [1:length(transitionSequence);],
-        startvalue = 1,format = x -> string(transitionSequence[x]))
+        (
+            label = "Transition",
+            range = [1:length(transitionSequence);],
+            startvalue = 1,
+            format = x -> string(transitionSequence[x]),
+        ),
     )
     currentTransition = lift(sliderTransition.sliders[1].value) do val
         return transitionSequence[val]
-     end
-    
+    end
 
-     scatter!(lsceneLeft, lift(x->transitionRefPositions[x], currentTransition) ;markersize=lift(x->abs.(transitionInvariants1[x]) * 70, currentTransition),
-      color=lift(x->transitionInvariants1[x], currentTransition), colormap=:bwr, colorrange=(-0.4,0.4))
 
-     scatter!(lsceneRight, lift(x->transitionRefPositions[x], selectedTransition) ;markersize=lift(x->abs.(transitionInvariants1[x]) * 70, selectedTransition),
-      color=lift(x->transitionInvariants1[x], selectedTransition), colormap=:bwr, colorrange=(-0.4,0.4))
+    scatter!(
+        lsceneLeft,
+        lift(x -> transitionRefPositions[x], currentTransition);
+        markersize = lift(x -> abs.(transitionInvariants1[x]) * 70, currentTransition),
+        color = lift(x -> transitionInvariants1[x], currentTransition),
+        colormap = :bwr,
+        colorrange = (-0.4, 0.4),
+    )
 
-     stem!( axI1, atoms, lift(x->transitionInvariants1[x], selectedTransition))
-     stem!( axI1, atoms, lift(x->transitionInvariants1[x], currentTransition))
+    scatter!(
+        lsceneRight,
+        lift(x -> transitionRefPositions[x], selectedTransition);
+        markersize = lift(x -> abs.(transitionInvariants1[x]) * 70, selectedTransition),
+        color = lift(x -> transitionInvariants1[x], selectedTransition),
+        colormap = :bwr,
+        colorrange = (-0.4, 0.4),
+    )
 
-     stem!( axI2, atoms, lift(x->transitionInvariants2[x], selectedTransition))
-     stem!( axI2, atoms, lift(x->transitionInvariants2[x], currentTransition))
+    stem!(axI1, atoms, lift(x -> transitionInvariants1[x], selectedTransition))
+    stem!(axI1, atoms, lift(x -> transitionInvariants1[x], currentTransition))
 
-     stem!( axI3, atoms, lift(x->transitionInvariants3[x], selectedTransition))
-     stem!( axI3, atoms, lift(x->transitionInvariants3[x], currentTransition))
+    stem!(axI2, atoms, lift(x -> transitionInvariants2[x], selectedTransition))
+    stem!(axI2, atoms, lift(x -> transitionInvariants2[x], currentTransition))
+
+    stem!(axI3, atoms, lift(x -> transitionInvariants3[x], selectedTransition))
+    stem!(axI3, atoms, lift(x -> transitionInvariants3[x], currentTransition))
 
 
     link_cameras_lscene(molWindow)
--- src/io.jl
+++ src/io.jl
@@ -1,13 +1,17 @@
-function getDataSets( stateDataPath::String, sqeuencePath::String, transitionLabelPath::String )::Dict{String, Dict}
-    
-    combinedData = Dict{String, Dict}() # combined set
-    transitionInvariants1 = Dict{String,Vector}() 
-    transitionInvariants2 = Dict{String,Vector}() 
-    transitionInvariants3 = Dict{String,Vector}() 
-    atomPositions = Dict{String, Matrix}()
-    distanceMatrices = Dict{String, Matrix}()
-    transitionRefPositions = Dict{String, Vector}()
-    transitionLabels = Dict{String, Int64}()
+function getDataSets(
+    stateDataPath::String,
+    sqeuencePath::String,
+    transitionLabelPath::String,
+)::Dict{String,Dict}
+
+    combinedData = Dict{String,Dict}() # combined set
+    transitionInvariants1 = Dict{String,Vector}()
+    transitionInvariants2 = Dict{String,Vector}()
+    transitionInvariants3 = Dict{String,Vector}()
+    atomPositions = Dict{String,Matrix}()
+    distanceMatrices = Dict{String,Matrix}()
+    transitionRefPositions = Dict{String,Vector}()
+    transitionLabels = Dict{String,Int64}()
 
 
     transitionLabelData = Pickle.npyload(transitionLabelPath)
@@ -16,7 +20,7 @@ function getDataSets( stateDataPath::String, sqeuencePath::String, transitionLab
         source = key |> first |> string
         target = key |> last |> string
 
-        name = source *">"*target
+        name = source * ">" * target
         transitionLabels[name] = value
     end
 
@@ -27,9 +31,9 @@ function getDataSets( stateDataPath::String, sqeuencePath::String, transitionLab
     rootPath = dirname(dirname(@__FILE__))
     println("Root directory is: $(rootPath)")
 
-    if isfile("$(rootPath)/cache/transitionInvariants1_$(sequenceHash).jld2") 
+    if isfile("$(rootPath)/cache/transitionInvariants1_$(sequenceHash).jld2")
         println("Found precomputed data, loading data...")
-        
+
         @time atomPositions = JLD2.jldopen(
             "$(rootPath)/cache/atomPositions_$(sequenceHash).jld2";
             compress = true,
@@ -51,7 +55,7 @@ function getDataSets( stateDataPath::String, sqeuencePath::String, transitionLab
         ) do file
             file["distanceMatrices"]
         end
-        
+
         @time transitionInvariants1 = JLD2.jldopen(
             "$(rootPath)/cache/transitionInvariants1_$(sequenceHash).jld2";
             compress = true,
@@ -79,22 +83,47 @@ function getDataSets( stateDataPath::String, sqeuencePath::String, transitionLab
         println("Reading dataset....")
         distanceMatrices = loadDistanceMatricesFromData(stateDataPath)
         atomPositions = loadAtomPositionsFromData(stateDataPath)
-    
-        (transitionInvariants1, transitionInvariants2, transitionInvariants3) = computeTransitionInvariants( sequence, atomPositions, distanceMatrices ) 
+
+        (transitionInvariants1, transitionInvariants2, transitionInvariants3) =
+            computeTransitionInvariants(sequence, atomPositions, distanceMatrices)
 
         println("Storing  data.... $(rootPath)/cache/$(sequenceHash).jld2")
 
-        @time JLD2.jldsave("$(rootPath)/cache/atomPositions_$(sequenceHash).jld2", true; atomPositions)
-        @time JLD2.jldsave("$(rootPath)/cache/transitionRefPositions_$(sequenceHash).jld2", true; transitionRefPositions)
-        @time JLD2.jldsave("$(rootPath)/cache/distanceMatrices_$(sequenceHash).jld2", true; distanceMatrices)
-        @time JLD2.jldsave("$(rootPath)/cache/transitionInvariants1_$(sequenceHash).jld2", true; transitionInvariants1)
-        @time JLD2.jldsave("$(rootPath)/cache/transitionInvariants2_$(sequenceHash).jld2", true; transitionInvariants2)
-        @time JLD2.jldsave("$(rootPath)/cache/transitionInvariants3_$(sequenceHash).jld2", true; transitionInvariants3)
-        
+        @time JLD2.jldsave(
+            "$(rootPath)/cache/atomPositions_$(sequenceHash).jld2",
+            true;
+            atomPositions,
+        )
+        @time JLD2.jldsave(
+            "$(rootPath)/cache/transitionRefPositions_$(sequenceHash).jld2",
+            true;
+            transitionRefPositions,
+        )
+        @time JLD2.jldsave(
+            "$(rootPath)/cache/distanceMatrices_$(sequenceHash).jld2",
+            true;
+            distanceMatrices,
+        )
+        @time JLD2.jldsave(
+            "$(rootPath)/cache/transitionInvariants1_$(sequenceHash).jld2",
+            true;
+            transitionInvariants1,
+        )
+        @time JLD2.jldsave(
+            "$(rootPath)/cache/transitionInvariants2_$(sequenceHash).jld2",
+            true;
+            transitionInvariants2,
+        )
+        @time JLD2.jldsave(
+            "$(rootPath)/cache/transitionInvariants3_$(sequenceHash).jld2",
+            true;
+            transitionInvariants3,
+        )
+
         println("Storing successfull")
     end
 
-    combinedData = Dict{String, Dict}() # combined set    
+    combinedData = Dict{String,Dict}() # combined set    
 
     combinedData["transitionInvariants1"] = transitionInvariants1
     combinedData["transitionInvariants2"] = transitionInvariants2
@@ -108,15 +137,15 @@ function getDataSets( stateDataPath::String, sqeuencePath::String, transitionLab
 end
 
 
-function loadAtomPositionsFromData( stateDataPath::String )::Dict{String, Matrix{Float64}}
+function loadAtomPositionsFromData(stateDataPath::String)::Dict{String,Matrix{Float64}}
     stateFiles = readdir(stateDataPath)
-    filter!(e->e ≠ ".DS_Store",stateFiles) # MacOS weirdness...
-    filter!(e->e ≠ "seq.txt",stateFiles) # filter sequence
-    filter!(e->!occursin("distance_matrix", e), stateFiles) # filter distances
+    filter!(e -> e ≠ ".DS_Store", stateFiles) # MacOS weirdness...
+    filter!(e -> e ≠ "seq.txt", stateFiles) # filter sequence
+    filter!(e -> !occursin("distance_matrix", e), stateFiles) # filter distances
 
     @show length(stateFiles)
 
-    positionData = Dict{String, Matrix}()
+    positionData = Dict{String,Matrix}()
 
     addStateToDict.(Ref(positionData), Ref(stateDataPath), stateFiles)
 
@@ -127,15 +156,15 @@ function loadAtomPositionsFromData( stateDataPath::String )::Dict{String, Matrix
 end
 
 
-function loadDistanceMatricesFromData( stateDataPath::String )::Dict{String, Matrix{Float64}}
+function loadDistanceMatricesFromData(stateDataPath::String)::Dict{String,Matrix{Float64}}
     stateFiles = readdir(stateDataPath)
-    filter!(e->e ≠ ".DS_Store",stateFiles) # MacOS weirdness...
-    filter!(e->e ≠ "seq.txt",stateFiles) # filter sequence
-    filter!(e->!occursin("positions", e), stateFiles) # filter positions
+    filter!(e -> e ≠ ".DS_Store", stateFiles) # MacOS weirdness...
+    filter!(e -> e ≠ "seq.txt", stateFiles) # filter sequence
+    filter!(e -> !occursin("positions", e), stateFiles) # filter positions
 
     @show length(stateFiles)
 
-    distanceMatrices = Dict{String, Matrix}()
+    distanceMatrices = Dict{String,Matrix}()
 
     addStateToDict.(Ref(distanceMatrices), Ref(stateDataPath), stateFiles)
 
@@ -146,20 +175,19 @@ function loadDistanceMatricesFromData( stateDataPath::String )::Dict{String, Mat
 end
 
 
-function addPositionsToDict( dict::Dict{String, Matrix}, pathToFile::String, fileName::String)
-    stateId =SubString( fileName, 1:((findfirst("_", fileName ) |> first) - 1) )
-    dict[stateId] = Pickle.npyload(open(pathToFile*fileName))
+function addPositionsToDict(dict::Dict{String,Matrix}, pathToFile::String, fileName::String)
+    stateId = SubString(fileName, 1:((findfirst("_", fileName)|>first)-1))
+    dict[stateId] = Pickle.npyload(open(pathToFile * fileName))
     return nothing
 end
 
 
-function addStateToDict( dict::Dict{String, Matrix}, pathToFile::String, fileName::String)
-    stateId =SubString( fileName, 1:((findfirst("_", fileName ) |> first) - 1) )
-    dict[stateId] = Pickle.npyload(open(pathToFile*fileName))
+function addStateToDict(dict::Dict{String,Matrix}, pathToFile::String, fileName::String)
+    stateId = SubString(fileName, 1:((findfirst("_", fileName)|>first)-1))
+    dict[stateId] = Pickle.npyload(open(pathToFile * fileName))
     return nothing
 end
 
-function getSequence( sqeuencePath::String )
-    return  readlines(sqeuencePath)
+function getSequence(sqeuencePath::String)
+    return readlines(sqeuencePath)
 end
-
