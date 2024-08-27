93980501d812af808fc1912e2773707cb258062b
Rostyslav Hnatyshyn <rostyslav.hnatyshyn@gmail.com>
2024-08-27 13:23:21 -0700

    dynamic colorbar for matrix plot

--- src/SelectionWindow.jl
+++ src/SelectionWindow.jl
@@ -1,80 +1,6 @@
 using Makie: clear_temporary_plots!, Orthographic, SparseArrays
 using StatsBase
 
-# leaving out non-! version for now
-function swarm_plot!(scene, bins, currently_selected, on_click)
-    x = Vector{Int}()
-    y = Vector{Int}()
-    colors = []
-
-    t_to_idx = Dict{Tuple{Int,Int},Int}()
-
-    idx = 1
-    for (binIdx, vals) in bins
-        # x val is binIdx
-        # y val is position in vals array
-        foreach(val -> push!(x, binIdx), vals)
-        foreach(val -> push!(colors, :blue), vals)
-        foreach(function (i)
-                t_to_idx[vals[i]] = idx
-                push!(y, i)
-                idx += 1
-            end, eachindex(vals))
-    end
-
-    obs_colors = Observable(colors)
-
-    sc = scatter!(scene, x, y, color=obs_colors)
-    sc.inspectable[] = false
-
-    inspector = DataInspector(scene)
-    on(events(scene).mouseposition) do mp
-        obs_colors[] = fill(:blue, length(obs_colors[]))
-
-        plot, idx = pick(scene)
-        if plot == sc
-            pos = position_on_plot(plot, idx)
-            if !isnan(pos)
-                # would need to get idx of bin
-                t = bins[Int(pos[1])][Int(pos[2])]
-
-                currently_selected[] = t
-                notify(currently_selected)
-
-                t_idx = t_to_idx[t]
-
-                obs_colors[][t_idx] = :red
-                inspector.plot.text[] = string(t)
-                inspector.plot.visible[] = true
-                inspector.plot.position = mp
-            end
-        end
-        notify(obs_colors)
-
-    end
-
-    on(events(scene).mousebutton) do event
-        if event.button == Mouse.left && event.action == Mouse.press
-            #plot, idx = pick(bp)
-            plot, idx = pick(scene)
-            pos = position_on_plot(plot, idx)
-            if !isnan(pos) && (plot == sc)
-                t = bins[Int(pos[1])][Int(pos[2])]
-                on_click(t, x -> ())
-            end
-        end
-        if event.button == Mouse.right && event.action == Mouse.press
-            reset_limits!(scene)
-        end
-
-        return Consume(false)
-    end
-    hidexdecorations!(scene, ticks=false, ticklabels=false)
-    hideydecorations!(scene, ticks=false, ticklabels=false)
-
-    return sc
-end
-
 function dist_plot!(scene, x_positions, y_positions, currently_selected, t_list, on_click, reference_configuration, x_label, y_label)
     positions = @lift begin
         pos = Vector{Point2f}()
@@ -213,8 +139,8 @@ function build_selection_window(fig_size,
     atom_scene = LScene(window[2, 1], show_axis=false,
         scenekw=scenekw = (backgroundcolor=:white, clear=true))
 
-    scene_2d = Axis(window[2, 2])
-    hidedecorations!(scene_2d)
+    matrix_scene = Axis(window[2, 2])
+    hidedecorations!(matrix_scene)
 
     selection_scene = Axis(window[3, :])
 
@@ -266,6 +192,7 @@ function build_selection_window(fig_size,
     minInvariant1, maxInvariant1, transitionInvariants1 = iv1
     t_list = collect(keys(transitionInvariants1))
 
+    # data, min, max
     processed_data = lift(x -> load_data(data[x]), selected_data)
     currently_selected = Observable{Any}(Nothing)
 
@@ -274,17 +201,17 @@ function build_selection_window(fig_size,
     mat_2d = @lift begin
         mat = zeros(1, 1)
         if $currently_selected != Nothing
-            mat = $processed_data[$currently_selected]
+            mat = $processed_data[1][$currently_selected]
         end
         return mat
-
     end
 
-    matrix_plot = heatmap!(scene_2d, mat_2d, colorrange=(0.0, 1.0), colormap=:viridis)
+    matrix_plot = heatmap!(matrix_scene, mat_2d, colorrange=lift(x -> (x[2], x[3]), processed_data), colormap=:viridis)
     matrix_plot.inspectable[] = false
+    Colorbar(window[1, 2], colormap=:viridis, limits=lift(x -> (x[2], x[3]), processed_data), vertical=false)
 
     on(mat_2d) do r
-        reset_limits!(scene_2d)
+        reset_limits!(matrix_scene)
     end
 
     atom_selection_view!(atom_scene, reference_configuration, selected_atoms, num_atoms, alignedPositions, transitionKDTree)
@@ -329,13 +256,16 @@ function load_data(data)
     # simple min-max norm
     max_val = maximum(map((x) -> maximum(x), d))
     min_val = minimum(map((x) -> minimum(x), d))
+
+    @show min_val, max_val
+
     norm = Dict{Tuple{Int,Int},Matrix}()
 
     for (transition, val) in data
         norm[transition] = (val .- min_val) / (max_val - min_val)
     end
 
-    return norm
+    return norm, min_val, max_val
 end
 
 function generate_points(data, matrix_shape, matrix_spacing, transition_spacing)
