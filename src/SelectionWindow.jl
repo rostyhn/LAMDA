c515beb5fce171c0384c66de722b675f4baf47cd
Rostyslav Hnatyshyn <rostyslav.hnatyshyn@gmail.com>
2025-02-28 13:56:30 -0700

    speed up hover on dist matrix

--- src/SelectionWindow.jl
+++ src/SelectionWindow.jl
@@ -112,7 +112,6 @@ function build_selection_window(fig_size,
         dendrogram!(graph_ax, $clustering, $h_cutoff; hover_callbackfn=on_dendrogram_hover)
     end
 
-
     dm_menu = Menu(window, options=collect(keys(dms)))
     on(dm_menu.selection) do val
         selected_dm[] = val
@@ -125,7 +124,7 @@ function build_selection_window(fig_size,
     return window
 end
 
-function simple_atom_view(scene, g, ap, t, order, scalars, sel)
+function simple_atom_view!(scene, g, ap, t, order, scalars, sel)
     opts = sort(collect(keys(scalars)))
 
     gg = GridLayout(g[end+1, :])
@@ -133,24 +132,19 @@ function simple_atom_view(scene, g, ap, t, order, scalars, sel)
 
     colorInfo = @lift begin
         opt = $(m.selection)
-        vals = scalars[opt][t][order]
+        vals = scalars[opt][$t][order]
 
         extremaVals = extrema(vals)
-        labelfn = (self, i, p) -> "Atom $(i); weight: $(self.color[][i])"
-
         # three cases:
         # sequential ascending, descending and diverging
         # divergent if we are approximately around 0 when subtracting the absolute values of the min and max
         minVal, maxVal = extremaVals
         if isapprox(abs(maxVal) - abs(minVal), 0; atol=1)
-            println("using divergent colorscheme")
             cmap = resample_cmap(:bam, 100; alpha=([(-0.99):0.02:(0.99);] ./ 0.1) .^ 6)
         else
             # ascending sequential if min is closer to 0
             cmap = resample_cmap(:reds, 147, alpha=range(; start=0.01, stop=1.0, length=147)) #seq ascending
             if abs(minVal) > abs(maxVal)
-                println("using descending sequential colorscheme")
-                # otherwise reverse it
                 reverse!(cmap)
             end
         end
@@ -158,21 +152,20 @@ function simple_atom_view(scene, g, ap, t, order, scalars, sel)
         sel[] = opt
         notify(sel)
 
-        return vals, extremaVals, cmap, labelfn
+        return vals, extremaVals, cmap
     end
 
-    scatter!(scene, ap, color=lift(x -> x[1], colorInfo),
+    scatter!(scene, lift((x, y) -> x[y][order], ap, t),
+        color=lift(x -> x[1], colorInfo),
         colorrange=lift(x -> x[2], colorInfo),
         colormap=lift(x -> x[3], colorInfo),
-        inspector_label=lift(x -> x[4], colorInfo),
+        inspector_label=(self, i, p) -> "Atom $(i); weight: $(self.color[][i])",
         markersize=30)
 
     cbar = Colorbar(gg[1, 2], colorrange=lift(x -> x[2], colorInfo), vertical=false, colormap=lift(x -> x[3], colorInfo), tellwidth=false)
 
     return [], [(gg, [m, cbar])]
 end
-
-
 
 function setup_transition_view(fig, parentGrid, loc, ap, hovered, scalars, sampleRanges, volData, vol_cmap, volumeRange, t_to_idx)
     rootScene = LScene(
@@ -189,7 +182,7 @@ function setup_transition_view(fig, parentGrid, loc, ap, hovered, scalars, sampl
     g = vgrid!(m, rootScene, l)
     parentGrid[i, j] = g
 
-    inspector = DataInspector(rootScene)
+    DataInspector(rootScene)
 
     sel = Observable("Volume")
     bp_sel = Observable("")
@@ -198,8 +191,13 @@ function setup_transition_view(fig, parentGrid, loc, ap, hovered, scalars, sampl
     scene_listeners = Vector{Any}()
     ui_elements = Vector{Any}()
 
-    function cleanup_t_view()
-        cam = camera(rootScene)
+    on(m.selection) do cw
+        sel[] = cw
+        notify(sel)
+    end
+
+    @lift begin
+        # cleanup
         empty!(rootScene)
 
         for listener in scene_listeners
@@ -222,24 +220,12 @@ function setup_transition_view(fig, parentGrid, loc, ap, hovered, scalars, sampl
             end
         end
         Makie.trim!(g)
-        #GC.gc() # stops memleak
-    end
 
-    menu_listener = nothing
+        if $sel == "Volume"
+            vd = lift((x, y, z) ->
+                    reshape(x[:, t_to_idx[y]], (length(z[1]), length(z[2]), length(z[3]))), volData, hovered, sampleRanges)
 
-    @lift begin
-        bp = function (scene, inspector, g)
-            return simple_atom_view(scene, g, $ap[$hovered][1], $hovered, 1, scalars, bp_sel)
-        end
-
-        afp = function (scene, inspector, g)
-            return simple_atom_view(scene, g, $ap[$hovered][2], $hovered, 2, scalars, ap_sel)
-        end
-
-        vol = function (scene, inspector, g)
-            vd = reshape($volData[:, t_to_idx[$hovered]], (length($sampleRanges[1]), length($sampleRanges[2]), length($sampleRanges[3])))
-
-            v = volume!(scene,
+            v = volume!(rootScene,
                 lift(x -> extrema(x[1]), sampleRanges),
                 lift(x -> extrema(x[2]), sampleRanges),
                 lift(x -> extrema(x[3]), sampleRanges),
@@ -253,22 +239,13 @@ function setup_transition_view(fig, parentGrid, loc, ap, hovered, scalars, sampl
                 visible=true)
             v.inspectable[] = false
 
-            return [], []
+            il = []
+            is = []
+        elseif $sel == "Initial State"
+            il, is = simple_atom_view!(rootScene, g, ap, hovered, 1, scalars, bp_sel)
+        else
+            il, is = simple_atom_view!(rootScene, g, ap, hovered, 2, scalars, ap_sel)
         end
-
-        rf = Dict()
-        rf["Initial State"] = bp
-        rf["Final State"] = afp
-        rf["Volume"] = vol
-
-        cleanup_t_view()
-        if !isnothing(menu_listener)
-            off(menu_listener)
-            menu_listener = nothing
-        end
-
-        initial_render_func = rf[$sel]
-        il, is = initial_render_func(rootScene, inspector, g)
 
         for l in il
             push!(scene_listeners, l)
@@ -276,23 +253,6 @@ function setup_transition_view(fig, parentGrid, loc, ap, hovered, scalars, sampl
 
         for s in is
             push!(ui_elements, s)
-        end
-
-        menu_listener = on(m.selection) do cw
-            cleanup_t_view()
-
-            listeners, scenes = rf[cw](rootScene, inspector, g)
-
-            for l in listeners
-                push!(scene_listeners, l)
-            end
-
-            for s in scenes
-                push!(ui_elements, s)
-            end
-            update_cam!(rootScene.scene)
-            sel[] = cw
-            notify(sel)
         end
     end
 
